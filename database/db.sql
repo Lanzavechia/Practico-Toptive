5734b311d9d314728bb0628fa82b8ea6580e24dd
Agustin Lanzavecchia <38304039+Lanzavechia@users.noreply.github.com>
2020-05-05 21:07:54 -0300

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE calculadora;
 Use calculadora;
 
 CREATE TABLE historial(
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY;
-    calculo VARCHAR(100) NOT NULL;
-    resultado VARCHAR(100) NOT NULL;
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    calculo VARCHAR(100) NOT NULL,
+    resultado VARCHAR(100) NOT NULL,
 )
